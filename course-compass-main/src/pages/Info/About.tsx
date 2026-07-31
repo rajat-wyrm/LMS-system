079fe9876aef933636cf5a00736abcd4bcf72024
Spoorthi Kothapalli <spporthikothapalli@gmail.com>
2026-07-31 15:10:21 +0530

    Fix footer links and add placeholder pages

---
+++ course-compass-main/src/pages/Info/About.tsx
@@ -0,0 +1,12 @@
+import InfoPlaceholder from "@/pages/Info/InfoPlaceholder";
+
+const About = () => {
+  return (
+    <InfoPlaceholder
+      title="About UpToSkills"
+      description="We are putting together a deeper look at our mission, mentors, and learning approach. This page will be expanded soon."
+    />
+  );
+};
+
+export default About;
